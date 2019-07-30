7c621d79a80cbf8ff813acfdc8d2fe81b90d4435
michael <michael@mbh.sh>
2019-07-30 17:31:56 +0100

    add cluster-status script

---
+++ {{cookiecutter.profile_name}}/lsf-status.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import time
+from typing import Tuple
+
+WAIT_BETWEEN_TRIES = 5
+TRY_TIMES = 3
+SUCCESS = "success"
+RUNNING = "running"
+FAILED = "failed"
+STATUS_TABLE = {
+    "PEND": RUNNING,
+    "RUN": RUNNING,
+    "DONE": SUCCESS,
+    "PSUSP": RUNNING,
+    "USUSP": RUNNING,
+    "SSUSP": RUNNING,
+    "WAIT": RUNNING,
+    "EXIT": FAILED,
+    "POST_DONE": SUCCESS,
+    "POST_ERR": FAILED,
+    "UNKWN": RUNNING,
+}
+
+
+def get_job_status(stdout: str) -> str:
+    fields = stdout.split()
+    status = fields[2]
+
+    return status
+
+
+def get_status_for_snakemake(job_status: str) -> str:
+    status = STATUS_TABLE.get(job_status, "unknown")
+
+    if status == "unknown":
+        print(
+            "Got an unknown job status {}. Defaulting to '{}'...".format(
+                job_status, FAILED
+            ),
+            file=sys.stderr,
+        )
+        status = FAILED
+
+    return status
+
+
+def query_status(jobid: int) -> Tuple[str, str]:
+    cmd = "bjobs -noheader {}".format(jobid)
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    out, err = proc.communicate()
+
+    return out.decode().strip(), err.decode().strip()
+
+
+def query_status_failed(stderr: str, jobid: int) -> bool:
+    return stderr.startswith("Job <{}> is not found".format(jobid))
+
+
+def main():
+    jobid = int(sys.argv[1])
+
+    stdout, stderr = query_status(jobid)
+
+    tries = 0
+    while query_status_failed(stderr, jobid) and tries < TRY_TIMES:
+        time.sleep(WAIT_BETWEEN_TRIES)
+        stdout, stderr = query_status(jobid)
+        tries += 1
+
+    job_status = get_job_status(stdout)
+    status_for_snakemake = get_status_for_snakemake(job_status)
+
+    print(status_for_snakemake)
+
+
+if __name__ == "__main__":
+    main()
