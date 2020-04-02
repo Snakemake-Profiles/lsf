fbe690d5716b5db2f5c0e1a312d1090863e29d1d
Michael Hall <mbhall88@gmail.com>
2020-04-02 11:50:40 +0100

    format and rename variables

--- {{cookiecutter.profile_name}}/lsf_status.py
+++ {{cookiecutter.profile_name}}/lsf_status.py
@@ -5,6 +5,7 @@ from typing import List
 from subprocess import CalledProcessError
 import sys
 from pathlib import Path
+
 if not __name__.startswith("tests.src."):
     sys.path.append(str(Path(__file__).parent.absolute()))
     from OSLayer import OSLayer
@@ -34,11 +35,17 @@ class LSF_Status_Checker:
         "UNKWN": RUNNING,
     }
 
-    def __init__(self, jobid: int, outlog: str, WAIT_BETWEEN_TRIES: float = 5, TRY_TIMES: int = 3):
+    def __init__(
+        self,
+        jobid: int,
+        outlog: str,
+        wait_between_tries: float = 5,
+        max_status_checks: int = 3,
+    ):
         self._jobid = jobid
         self._outlog = outlog
-        self.WAIT_BETWEEN_TRIES = WAIT_BETWEEN_TRIES
-        self.TRY_TIMES = TRY_TIMES
+        self.wait_between_tries = wait_between_tries
+        self.max_status_checks = max_status_checks
 
     @property
     def jobid(self) -> int:
@@ -57,7 +64,11 @@ class LSF_Status_Checker:
 
         stdout_is_empty = not output_stream.strip()
         if stdout_is_empty:
-            raise BjobsError("bjobs error.\nstdout is empty.\nstderr = {stderr}".format(stderr=error_stream))
+            raise BjobsError(
+                "bjobs error.\nstdout is empty.\nstderr = {stderr}".format(
+                    stderr=error_stream
+                )
+            )
 
         return self.STATUS_TABLE[output_stream]
 
@@ -69,9 +80,14 @@ class LSF_Status_Checker:
     def _query_status_using_log(self) -> str:
         try:
             log_lines = self._get_lines_of_log_file()
-            resource_summary_usage_line_index = log_lines.index("Resource usage summary:")
+            resource_summary_usage_line_index = log_lines.index(
+                "Resource usage summary:"
+            )
             status_line = log_lines[resource_summary_usage_line_index - 2]
-            assert status_line.startswith("Exited with exit code") or status_line == "Successfully completed."
+            assert (
+                status_line.startswith("Exited with exit code")
+                or status_line == "Successfully completed."
+            )
             if status_line == "Successfully completed.":
                 return self.SUCCESS
             else:
@@ -81,26 +97,44 @@ class LSF_Status_Checker:
 
     def get_status(self) -> str:
         status = None
-        for _ in range(self.TRY_TIMES):
+        for _ in range(self.max_status_checks):
             try:
                 status = self._query_status_using_bjobs()
                 break  # succeeded on getting the status
             except BjobsError as error:
-                print("[Predicted exception] BjobsError: {error}".format(error=error), file=sys.stderr)
+                print(
+                    "[Predicted exception] BjobsError: {error}".format(error=error),
+                    file=sys.stderr,
+                )
                 print("Resuming...", file=sys.stderr)
-                time.sleep(self.WAIT_BETWEEN_TRIES)
+                time.sleep(self.wait_between_tries)
             except KeyError as error:
-                print("[Predicted exception] Unknown job status: {error}".format(error=error), file=sys.stderr)
+                print(
+                    "[Predicted exception] Unknown job status: {error}".format(
+                        error=error
+                    ),
+                    file=sys.stderr,
+                )
                 print("Resuming...", file=sys.stderr)
-                time.sleep(self.WAIT_BETWEEN_TRIES)
+                time.sleep(self.wait_between_tries)
             except CalledProcessError as error:
-                print("[Predicted exception] Error on calling bjobs: {error}".format(error=error), file=sys.stderr)
+                print(
+                    "[Predicted exception] Error on calling bjobs: {error}".format(
+                        error=error
+                    ),
+                    file=sys.stderr,
+                )
                 print("Resuming...", file=sys.stderr)
-                time.sleep(self.WAIT_BETWEEN_TRIES)
+                time.sleep(self.wait_between_tries)
 
         bjobs_failed = status is None
         if bjobs_failed:
-            print("bjobs failed {try_times} times. Checking log...".format(try_times=self.TRY_TIMES), file=sys.stderr)
+            print(
+                "bjobs failed {try_times} times. Checking log...".format(
+                    try_times=self.max_status_checks
+                ),
+                file=sys.stderr,
+            )
             status = self._query_status_using_log()
 
         return status
