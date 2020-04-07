a2e340b7319ac86de34fd30b86c720d1027ad4f3
Michael Hall <mbhall88@gmail.com>
2020-04-07 16:08:51 +0100

    add concat params, get, and default params

--- tests/test_lsf_config.py
+++ tests/test_lsf_config.py
@@ -23,3 +23,100 @@ class TestContains:
         config = Config.from_stream(stream)
 
         assert item not in config
+
+
+class TestConcatenateParams:
+    def test_str_returns_str(self):
+        params = "-q queue"
+
+        actual = Config.concatenate_params(params)
+        expected = params
+
+        assert actual == expected
+
+    def test_empty_list_returns_empty_str(self):
+        params = []
+
+        actual = Config.concatenate_params(params)
+        expected = ""
+
+        assert actual == expected
+
+    def test_list_returns_str(self):
+        params = ["-q queue", "-P project"]
+
+        actual = Config.concatenate_params(params)
+        expected = "-q queue -P project"
+
+        assert actual == expected
+
+
+class TestGet:
+    def test_get_empty_returns_default(self):
+        stream = StringIO("")
+        config = Config.from_stream(stream)
+        key = "key"
+
+        actual = config.get(key)
+
+        assert actual is None
+
+    def test_get_key_in_yaml(self):
+        stream = StringIO("key: 'foo'")
+        key = "key"
+        config = Config.from_stream(stream)
+
+        actual = config.get(key)
+        expected = "foo"
+
+        assert actual == expected
+
+    def test_get_key_not_in_yaml_returns_default(self):
+        stream = StringIO("key: 'foo'")
+        key = "bar"
+        default = "default"
+        config = Config.from_stream(stream)
+
+        actual = config.get(key, default)
+        expected = default
+
+        assert actual == expected
+
+
+class TestDefaultParams:
+    def test_no_default_returns_empty(self):
+        stream = StringIO("key: 'foo'")
+        config = Config.from_stream(stream)
+
+        actual = config.default_params()
+        expected = ""
+
+        assert actual == expected
+
+    def test_default_present_returns_params(self):
+        stream = StringIO("__default__: '-q foo'")
+        config = Config.from_stream(stream)
+
+        actual = config.default_params()
+        expected = "-q foo"
+
+        assert actual == expected
+
+    def test_default_present_params_are_list_returns_params(self):
+        stream = StringIO("__default__:\n  - '-q foo'\n  - '-P project'")
+        config = Config.from_stream(stream)
+
+        actual = config.default_params()
+        expected = "-q foo -P project"
+
+        assert actual == expected
+
+    def test_default_present_without_underscores_returns_empty(self):
+        stream = StringIO("default:\n  - '-q foo'\n  - '-P project'")
+        config = Config.from_stream(stream)
+
+        actual = config.default_params()
+        expected = ""
+
+        assert actual == expected
+
