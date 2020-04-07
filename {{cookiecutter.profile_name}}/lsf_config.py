ea56e44afedea790dc9be77ec19e0e7fd0e30e7b
Michael Hall <mbhall88@gmail.com>
2020-04-07 16:08:51 +0100

    add concat params, get, and default params

--- {{cookiecutter.profile_name}}/lsf_config.py
+++ {{cookiecutter.profile_name}}/lsf_config.py
@@ -1,16 +1,31 @@
-from typing import TextIO
+from typing import TextIO, Union, List, Any
 
 import yaml
 
 
 class Config:
-    def __init__(self, data: yaml.YAMLObject):
+    def __init__(self, data: dict):
         self._data = data
 
     def __contains__(self, item) -> bool:
         return item in self._data
 
+    def get(self, key: str, default: Any = None) -> Any:
+        return self._data.get(key, default)
+
+    @staticmethod
+    def concatenate_params(params: Union[List[str], str]) -> str:
+        if isinstance(params, str):
+            return params
+        return " ".join(params)
+
+    def default_params(self) -> str:
+        return self.concatenate_params(self.get("__default__", ""))
+
+    def params_for_rule(self, rulename: str) -> str:
+        pass
+
     @staticmethod
     def from_stream(stream: TextIO) -> "Config":
         data = yaml.safe_load(stream)
-        return Config(data)
+        return Config(data) if data is not None else dict()
